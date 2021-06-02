c8b627229a7fdd079ef020097d4617e5a9a89f65
Rino Cala <rc50553@fer.hr>
2021-06-03 01:50:21 +0200

    Implemented BLSTM, test acc for BERT, bow, char ngram and glove for classic models

--- main_deep.py
+++ main_deep.py
@@ -1,3 +1,8 @@
+
+
+"""
+#BERT
+ 
 import utils_general
 from utils_deep import QuoraDataBert, train_deep
 from transformers import BertForSequenceClassification
@@ -20,4 +25,56 @@ model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_l
 model.to(device)
 optimizer = Adam(model.parameters(), lr=2e-5)
 epochs = 3
-train_deep(model, train_loader, val_loader, optimizer, epochs, device)
+train_deep(model, train_loader, val_loader, test_loader, optimizer, epochs, device)
+
+"""
+
+#BLSTM
+
+import utils_general
+import utils_classic
+import utils_deep
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+import pandas as pd
+
+"""
+X_dataset, y_dataset = utils_general.get_Xy_train(utils_general.load_dataset_numpy("train.csv"))
+X_dataset, y_dataset = utils_general.rmv_float_and_nan(X_dataset, y_dataset)
+X_dataset = utils_general.remove_punctuation(X_dataset)
+X_train, y_train, X_val, y_val, X_test, y_test = utils_general.split_train_dev_test(
+    X_dataset, y_dataset
+)
+"""
+
+dataset=pd.read_csv("train.csv").to_numpy()
+dataset_length=len(dataset)
+ratios=[0.7,0.15,0.15]
+vocab_text=utils_general.Vocab("train.csv",False)
+vocab_label=utils_general.Vocab("train.csv",True)
+embedding_matrix=utils_deep.generate_embedding_matrix('sst_glove_6b_300d.txt',vocab_text)
+train_dataset=utils_deep.NLPDataset(vocab_text,vocab_label,"train.csv",[0,int(ratios[0]*dataset_length)])
+val_dataset=utils_deep.NLPDataset(vocab_text,vocab_label,"train.csv",[int(ratios[0]*dataset_length),int((ratios[0]+ratios[1])*dataset_length)])
+test_dataset=utils_deep.NLPDataset(vocab_text,vocab_label,"train.csv",[int((ratios[0]+ratios[1])*dataset_length),dataset_length])
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=32, 
+                              shuffle=True, collate_fn=utils_deep.pad_collate_fn)
+valid_dataloader = DataLoader(dataset=val_dataset, batch_size=32, 
+                              shuffle=True, collate_fn=utils_deep.pad_collate_fn)
+test_dataloader = DataLoader(dataset=test_dataset, batch_size=32, 
+                              shuffle=True, collate_fn=utils_deep.pad_collate_fn)
+
+model=utils_deep.BLSTM(embedding_matrix)
+criterion=nn.BCEWithLogitsLoss()
+optimizer=Adam(model.get_params(),0.005)
+epochs=2
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+for epoch in range(epochs):
+    utils_deep.train(model,train_dataloader,optimizer,criterion,device)
+    metrics=utils_deep.evaluate(model,valid_dataloader,criterion,device)
+    print("Valid Loss {}, Acc {}".format(metrics['loss'],metrics['acc']))
+metrics=utils_deep.evaluate(model,test_dataloader,criterion,device)
+print("Test Loss {}, Acc {}".format( metrics['loss'],metrics['acc']))
--- utils_deep.py
+++ utils_deep.py
@@ -1,15 +1,18 @@
 import torch
+from torch import nn
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from transformers import BertTokenizer
 import time
 from tqdm import tqdm
-
+import numpy as np
+from utils_general import Instance
+import pandas as pd
 
 class QuoraDataBert(Dataset):
     def __init__(self, datasets_x, datasets_y):
         self.tokenizer = BertTokenizer.from_pretrained(
-            "bert-base_uncased", do_lower_case=True
+            "bert-base-uncased", do_lower_case=True
         )
         self.datasets_x = datasets_x
         self.datasets_y = datasets_y
@@ -70,7 +73,7 @@ def acc_stat(y_pred, y_true):
     return acc
 
 
-def train_deep(model, train_loader, val_loader, optimizer, epochs, device):
+def train_deep(model, train_loader, val_loader, test_loader, optimizer, epochs, device):
 
     start = time.time()
 
@@ -87,7 +90,6 @@ def train_deep(model, train_loader, val_loader, optimizer, epochs, device):
 
             loss, prediction = model(
                 pair_token_ids,
-                mask_ids=mask_ids,
                 attention_mask=mask_ids,
                 labels=labels,
             ).values()
@@ -117,7 +119,6 @@ def train_deep(model, train_loader, val_loader, optimizer, epochs, device):
 
                 loss, prediction = model(
                     pair_token_ids,
-                    mask_ids=mask_ids,
                     attention_mask=mask_ids,
                     labels=labels,
                 ).values()
@@ -129,11 +130,160 @@ def train_deep(model, train_loader, val_loader, optimizer, epochs, device):
 
         val_acc = total_val_acc / len(val_loader)
         val_loss = total_val_loss / len(val_loader)
+
+        total_test_acc = 0
+        total_test_loss = 0
+
+        with torch.no_grad():
+            for (pair_token_ids, mask_ids, seg_ids, y) in tqdm(test_loader):
+                optimizer.zero_grad()
+                pair_token_ids = pair_token_ids.to(device)
+                mask_ids = mask_ids.to(device)
+                seg_ids = seg_ids.to(device)
+                labels = y.to(device)
+
+                loss, prediction = model(
+                    pair_token_ids,
+                    attention_mask=mask_ids,
+                    labels=labels,
+                ).values()
+
+                acc = acc_stat(prediction, labels)
+
+                total_test_loss += loss.item()
+                total_test_acc += acc.item()
+
+        test_acc = total_test_acc / len(test_loader)
+        test_loss = total_test_loss / len(test_loader)
+
         end = time.time()
         hours, rem = divmod(end - start, 3600)
         minutes, seconds = divmod(rem, 60)
 
         print(
-            f"Epoch {epoch+1}: train_loss: {train_loss:.4f} train_acc: {train_acc:.4f} | val_loss: {val_loss:.4f} val_acc: {val_acc:.4f}"
+            f"Epoch {epoch+1}: train_loss: {train_loss:.4f} train_acc: {train_acc:.4f} | val_loss: {val_loss:.4f} val_acc: {val_acc:.4f} \
+            | test_loss: {test_loss:.4f} val_acc: {test_acc:.4f}"
         )
         print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+
+class BLSTM(nn.Module):
+
+  def __init__(self,embedding_matrix,hidden_size=150,
+                num_layers=2,dropout=0,linear_dim=[300,150,1]):
+    super().__init__()
+    self.embedding_matrix=embedding_matrix
+    self.rnn=nn.LSTM(300,hidden_size=hidden_size,num_layers=num_layers,
+                        dropout=dropout,bidirectional=True)
+    self.fcs=[]
+    for i in range(len(linear_dim)-1):
+        self.fcs+=[nn.Linear(linear_dim[i],linear_dim[i+1],bias=True)]
+    self.fcs=nn.ModuleList(self.fcs)
+
+  def get_params(self):
+
+    params=list()
+
+    params.extend(self.embedding_matrix.parameters())
+    for i in range(len(self.fcs)):
+      params.extend(self.fcs[i].parameters())
+    params.extend(self.rnn.parameters())
+
+    return params
+
+  def forward(self, x):
+
+    x=self.embedding_matrix(x)
+    x = torch.transpose(x,0,1)
+    h,hidden=self.rnn(x,None)
+
+    h=h[-1]
+    for fc in self.fcs[:-1]:
+      h=fc(h)
+      h=torch.relu(h)
+    logits=self.fcs[-1](h)
+    return logits
+
+def pad_collate_fn(batch, pad_index=0):
+
+    texts, labels = zip(*batch) 
+    lengths = torch.tensor([len(text) for text in texts]) 
+    texts=pad_sequence(texts,batch_first=True,padding_value=pad_index)
+
+    return texts, labels, lengths
+
+def train(model, data, optimizer, criterion,device):
+  model.train()
+  for x, y, idxs in tqdm(data):
+    x=x.to(device)
+    y=torch.Tensor(y).to(device)
+    logits = model(x)
+    loss = criterion(logits, y.reshape(len(y),1))
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+def evaluate(model, data, criterion,device):
+  model.eval()
+  losses=[]
+  y_preds=[]
+  ys=[]
+  with torch.no_grad():
+    for x,y,idxs in data:
+      x=x.to(device)
+      y=torch.Tensor(y).to(device)
+      logits = model(x)
+      loss = criterion(logits, y.reshape(len(y),1))
+      losses+=[loss.cpu().detach().numpy()]
+      y_preds+=(logits>0).long().cpu().detach().numpy().tolist()
+      ys+=y.cpu().detach().numpy().tolist()
+    ys=np.array(ys)
+    y_preds=np.array(y_preds).squeeze()
+    acc=np.sum(ys==y_preds)/len(ys)
+    return {
+        "loss":np.mean(losses),
+        "acc":acc,
+    }
+
+def generate_embedding_matrix(file,vocab,random_normal=False,freeze=False):
+
+    f = open(file, "r")
+    embedding_dict={}
+    for line in f.read().splitlines():
+        word,embedding=line.split(" ",1)
+        embedding_dict[word]=[float(val) for val in embedding.split()]
+    
+    embedding_matrix=torch.randn(len(vocab.stoi),300)
+    embedding_matrix[0]=torch.zeros((300,))
+
+    if random_normal:
+        return torch.nn.Embedding.from_pretrained(embedding_matrix,padding_idx=0,freeze=freeze)
+
+    sorted_vocab=sorted(vocab.stoi.items(),key=lambda item: item[1])
+    
+    for idx, token in enumerate(sorted_vocab):
+        if embedding_dict.get(token[0]) is not None:
+            embedding_matrix[idx]=torch.Tensor(embedding_dict[token[0]])
+
+    return torch.nn.Embedding.from_pretrained(embedding_matrix,padding_idx=0,freeze=freeze)
+
+class NLPDataset(torch.utils.data.Dataset):
+
+    def __init__(self,vocab_text,vocab_label,file,range=None):
+        
+        self.instances=[]
+        self.vocab_text=vocab_text
+        self.vocab_label=vocab_label
+        dataset = pd.read_csv(file).to_numpy()
+        if range is not None:
+            dataset=dataset[range[0]:range[1]]
+        for line in dataset:
+            first,second,label=line[3:]
+            self.instances+=[Instance(str(first).split()+str(second).split(),str(label))]
+
+    def __getitem__(self,idx):
+        x_encoded=self.vocab_text.encode(self.instances[idx].text)
+        y_encoded=self.vocab_label.encode(self.instances[idx].label)
+        return x_encoded,y_encoded
+
+    def __len__(self):
+        return len(self.instances)
